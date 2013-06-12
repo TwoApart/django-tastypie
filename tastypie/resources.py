9fc3c620cb205f8470a6fe3642dc8f3c6c900af9
Javier Chamizo <jchamizo@twoapart.com>
2013-06-12 11:34:42 +0200

    Updating related_bundle when retrieved from DB

--- tastypie/resources.py
+++ tastypie/resources.py
@@ -2431,7 +2431,9 @@ class ModelResource(Resource):
                         request=bundle.request,
                         objects_saved=bundle.objects_saved
                     )
-                    related_resource.save(related_bundle)
+                    related_bundle = related_resource.save(related_bundle)
+                    related_obj = related_bundle.obj
+
 
                 setattr(bundle.obj, field_object.attribute, related_obj)
 
